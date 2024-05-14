0aa3359c36882f56569bf5a751e5fad9e47b4248
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-05-14 16:33:08 +0200

    Add toogle button to toolbar

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.weatherapp
 
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
-import android.widget.Toolbar
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.NetManager.Companion.checkAccessToNet
 
 
 class MainActivity : AppCompatActivity() {
-    
+
+    private var isChangedMetrics = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,29 +23,39 @@ class MainActivity : AppCompatActivity() {
 
         // Make toast with net status info
         if (checkAccessToNet(this)) {
-            Toast.makeText(this, "Połączony z internetem", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Internet connection !!", Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, "Brak połączenia z internetem", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show()
         }
     }
 
     // Function to inflate menu bar options
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
+        menuInflater.inflate(R.menu.menu,menu)
+
+        val toogleBtn = menu.findItem(R.id.switch_metrics_btn).actionView as ToggleButton
+        toogleBtn.text = "Metryki"
+
+        toogleBtn.setOnCheckedChangeListener{_ , isChecked ->
+            if (isChecked){
+                toogleBtn.textOn = "metryka1"
+                Toast.makeText(this,"Switch the metrics1",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                toogleBtn.textOff = "metryka2"
+                Toast.makeText(this,"Switch the metrics2",Toast.LENGTH_SHORT).show()
+            }
+        }
+
         return true
     }
 
     // Function to operate on items in toolbar
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection.
+        // Operate on option in toolbar
         return when (item.itemId) {
-            R.id.switch_metrics_btn -> {
-
-                Toast.makeText(this,"Switch the metrics",Toast.LENGTH_SHORT).show()
-                true
-            }
             R.id.refresh_data_btn -> {
+                Log.d("CHANGE IN MENU","Refresh the data")
                 Toast.makeText(this,"Refresh the data",Toast.LENGTH_SHORT).show()
                 true
             }
