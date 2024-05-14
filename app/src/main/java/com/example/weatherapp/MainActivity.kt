c1538c67d1c40d43efe8f60355d157643539b1eb
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-05-14 13:10:13 +0200

    Add class with function to check Internet connection

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.NetManager.Companion.checkAccessToNet
+
 
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Make toast with net status info
+        if (checkAccessToNet(this)) {
+            Toast.makeText(this, "Połączony z internetem", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Brak połączenia z internetem", Toast.LENGTH_SHORT).show()
         }
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/NetManager.kt
@@ -0,0 +1,27 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+class NetManager {
+
+    companion object {
+        fun checkAccessToNet(context: Context): Boolean {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val network = connectivityManager.activeNetwork
+                val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+                return networkCapabilities != null &&
+                        (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+            } else {
+                val networkInfo = connectivityManager.activeNetworkInfo
+                return networkInfo != null && networkInfo.isConnected
+            }
+        }
+    }
+}
