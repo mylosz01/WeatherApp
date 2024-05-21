70265901f7149cd95c809782510f46f104ec6a7d
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-05-21 14:08:45 +0200

    Add util object to store information about api and url

---
+++ app/src/main/java/com/example/weatherapp/Utils/Util.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapp.Utils
+
+object Utils {
+    const val BaseURL = "https://api.openweathermap.org/data/2.5/"
+    const val APIKey = "d00906ae38034eea1fd96baab88d0757"
+}
