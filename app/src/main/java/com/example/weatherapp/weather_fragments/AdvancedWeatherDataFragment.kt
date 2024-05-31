26dbe6c1f69dce03ea640e4704125e2b1aa8adeb
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-05-31 12:32:12 +0200

    Set value for AdvancedWeather

--- app/src/main/java/com/example/weatherapp/weather_fragments/AdvancedWeatherDataFragment.kt
+++ app/src/main/java/com/example/weatherapp/weather_fragments/AdvancedWeatherDataFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.weatherapp.R
+import com.example.weatherapp.weatherResponseData.CurrentWeatherResponseApi
 
 class AdvancedWeatherDataFragment : Fragment() {
 
@@ -14,7 +17,33 @@ class AdvancedWeatherDataFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_advanced_weather_data, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_advanced_weather_data, container, false)
 
+        val dataWeather = arguments?.getSerializable("data") as? CurrentWeatherResponseApi
+
+        if(dataWeather != null){
+            //set rain percent
+            view.findViewById<TextView>(R.id.rain_percent).text = (dataWeather.rain?.h.toString().toDoubleOrNull() ?: 0.0).toString()
+
+            //set wind speed
+            view.findViewById<TextView>(R.id.wind_speed).text = "${dataWeather.wind?.speed} m/s"
+
+            //set humidity
+            view.findViewById<TextView>(R.id.humidity).text = "${dataWeather.main?.humidity} %"
+
+            //set visibility
+            view.findViewById<TextView>(R.id.visibility).text = "${dataWeather.visibility} m"
+
+            //rotate image
+            view.findViewById<ImageView>(R.id.wind_direction_sign).rotation = dataWeather.wind?.deg!!.toFloat() + 90
+
+            //set wind degree
+            view.findViewById<TextView>(R.id.wind_degree).text = "${dataWeather.wind?.deg} °"
+
+            //set cloudiness
+            view.findViewById<TextView>(R.id.cloudiness).text = "${dataWeather.clouds?.all} %"
+        }
+
+        return view
+    }
 }
